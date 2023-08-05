29f638386ea43b667be8379ba459709fdbbbbf6b
Danila Kudashov <kudashovdanila@gmail.com>
2023-08-05 11:37:28 +0700

    configure correct routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Alerts from "./components/Alerts";
 import ErrorMsg from "./components/ErrorMsg";
 import Header from "./components/Header";
@@ -13,16 +13,38 @@ const App = () => {
 
 	return (
 		<BrowserRouter>
-			<Header />
-			<main className="flex justify-center items-center h-[100dvh] w-11/12 mx-auto">
-				<Routes>
-					<Route path="/" element={<Main />} />
-					<Route path="/settings" element={<Settings />} />
-					<Route path="/alerts/:addressWithSign" element={<Alerts />} />
-				</Routes>
-			</main>
-			{error && <ErrorMsg />}
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<>
+							<Header />
+							<main className="flex justify-center items-center h-[100dvh] w-11/12 mx-auto">
+								<Outlet />
+							</main>
+						</>
+					}
+				>
+					<Route index element={<Main />} />
+					<Route path="settings" element={<Settings />} />
+					{error && <ErrorMsg />}
+				</Route>
+
+				<Route path="alerts/:addressWithSign" element={<Alerts />} />
+			</Routes>
 		</BrowserRouter>
+
+		// <BrowserRouter>
+		// 	<Header />
+		// 	<main className="flex justify-center items-center h-[100dvh] w-11/12 mx-auto">
+		// 		<Routes>
+		// 			<Route path="/" element={<Main />} />
+		// 			<Route path="/settings" element={<Settings />} />
+		// 			<Route path="/alerts/:addressWithSign" element={<Alerts />} />
+		// 		</Routes>
+		// 	</main>
+		// 	{error && <ErrorMsg />}
+		// </BrowserRouter>
 	);
 };
 
